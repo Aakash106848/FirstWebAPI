c10c62c97a75ca2d89f0085c5686825f798217c3
Aakash106848 <aakash.b@gavstech.com>
2023-09-22 18:36:19 +0530

    APIClient

---
+++ EmployeeClient/EmployeeClient/EmployeeAPIClient.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPIClient
+{
+    internal class EmployeeAPIClient
+    {
+        static Uri uri = new Uri("http://localhost:5119");
+
+        public static async Task CallGetAllEmployees()
+        {
+            //58345
+            using (var client = new HttpClient()) 
+            {
+                client.BaseAddress = uri;
+                HttpResponseMessage response = await client.GetAsync("GetAllEmployee");
+                response.EnsureSuccessStatusCode();
+                if (response.IsSuccessStatusCode) 
+                {
+                    string x = await response.Content.ReadAsStringAsync();
+                    await Console.Out.WriteAsync(x);
+                }
+            }
+        }
+        public static async Task CallGetAllEmployeesJson() 
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = uri;
+                List<EmpViewModel> employees = new List<EmpViewModel>();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+                HttpResponseMessage response = await client.GetAsync("GetAllEmployee");
+                response.EnsureSuccessStatusCode();
+                if (response.IsSuccessStatusCode)
+                {
+                    string x = await response.Content.ReadAsStringAsync();
+                    employees = JsonConvert.DeserializeObject<List<EmpViewModel>>(x);
+                    foreach (EmpViewModel emp in employees)
+                    {
+                        await Console.Out.WriteLineAsync($"{emp.EmpId},{emp.FirstName},{emp.LastName},{emp.Title},{emp.City},{emp.HireDate},{emp.BirthDate},{emp.ReportsTo}");
+                    }
+                }
+            }
+        }
+    }   
+}
---
+++ EmployeeClient/EmployeeClient/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using WebAPIClient;
+
+Console.WriteLine("API Client");
+EmployeeAPIClient.CallGetAllEmployeesJson().Wait();
+//Task.WaitAll(EmployeeAPIClient.CallGetAllEmployees());
+
+Console.ReadLine();
